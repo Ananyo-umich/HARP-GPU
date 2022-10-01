3651294ae50e9c8027448a5586772b5fda505ab5
Cheng Li <chengcli@umich.edu>
2022-09-30 23:58:45 -0400

    add external library

--- cmake/setup_configure.cmake
+++ cmake/setup_configure.cmake
@@ -2,6 +2,24 @@
 ## set up model configuration ##
 ################################
 
+# precision
 set(Real float)
+
+# ghost zone size
+set(GhostZoneSize 3)
+
+# array size
+set(NumVapors 0)
+set(NumClouds 0)
+set(NumParticles 0)
+set(NumChemicalSpecies 0)
+set(NumTracers 0)
+
+# radiation stream
+set(NumStreams 4)
+set(NumUVBands 0)
+set(NumVISBands 0)
+set(NumIRBands 0)
+
 set(CrossDiskStatusBits 6)
 set(MaxReactants 5)
